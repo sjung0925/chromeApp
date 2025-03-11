cad0db8e6f8c29eb47bc81e960eeff0f8a55f1f3
jseo <adorable.jjae2@gmail.com>
2025-03-11 14:57:25 +0900

    클래스명 다루기, toggle 사용

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const h1 = document.querySelector(".title h1");
 
-function handleTitleClick() {
-    // js에서 css 건드리기 <- 비추
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
+function handleTitleClick() { // toggle
+    h1.classList.toggle("clicked");
+}
+
+function handleTitleClick_BETTER() { // classList
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        newColor = "blue";
+        h1.classList.add(clickedClass);
     }
-    h1.style.color = newColor;
+}
+
+function handleTitleClick_BUGS() {
+    const clickedClass = "clicked"; // 반복되는 비교 문자열 대응
+    // 만약에 클래스명이 두개 이상이라면?
+    if (h1.className === clickedClass) {
+        h1.className = ""; // 이 부분에서 clicked 외 유지해야하는 클래스명도 지워지는 버그 발생
+    } else {
+        h1.className = clickedClass;
+    }
 }
 
 h1.addEventListener("click", handleTitleClick);
