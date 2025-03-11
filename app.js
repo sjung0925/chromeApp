f01ecdf01a052f89ee65f178b7ffecfb6673b023
jseo <adorable.jjae2@gmail.com>
2025-03-11 14:17:45 +0900

    변수, 이벤트 사용해서 색깔 바꾸기(비추)

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
 const h1 = document.querySelector(".title h1");
 
 function handleTitleClick() {
-    h1.style.color = "blue";
+    // js에서 css 건드리기 <- 비추
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeaver() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS NO WIFI!");
-}
-
-function handleWindowOnline() {
-    alert("OH GOOD!");
-}
 h1.addEventListener("click", handleTitleClick);
-// h1.onclick = handeTitleClick; // 상동
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeaver);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
